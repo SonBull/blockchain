fafdf8ddb636dfbdd3c2b01db1d9b1a9c04e3413
Chris <cbmuc893@gmail.com>
2019-03-01 18:34:55 +0100

    created for loop,  blockchain can be filled with x elements

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,4 @@
-# Initialising our blockchain
+# Initialising our blockchain as empty list
 blockchain = []
 
 
@@ -8,8 +8,9 @@ def get_last_blockchain_value():
 
 
 def add_value(transaction_amount, last_transaction=[1]):
-    """ Append a new value as well as the last blockchain value to the blockchain.
-    
+    """ Append a new value as well as the last blockchain value 
+    to the blockchain.
+
     Arguments:
         :transaction_amount: The amount that should be added.
         :last_transaction: The last blockchain transaction (default [1])
@@ -17,17 +18,35 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
-    """ Returns the input of the user (a new transaction amount) as a float. """
-    return float(input('Your Transaction Amount please: '))
+def get_transaction_value():
+    """ Returns the input of the user (a new transaction amount) 
+    as a float. """
+    user_input = float(input('Your Transaction Amount please: '))
+    return user_input
 
-tx_amount = get_user_input()
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
+
+def print_blockchain_element():
+    # Output Blockchain
+    for block in blockchain:
+        print('Outputting Block')
+        print(block)
+
+# Get the first transaction element
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain_value())
-
-print(blockchain)
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_element()
+  
+print('Done!')
