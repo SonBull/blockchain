5fbd050c9b4f54f8b7e0bd54b571fdfe99766985
Chris <cbmuc893@gmail.com>
2019-03-08 17:45:07 +0100

    included verify function to check if the blockchain is valid

--- blockchain.py
+++ blockchain.py
@@ -39,12 +39,28 @@ def print_blockchain_element():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print('-' * 20)
 
+def verify_chain():
+    # block_index = 0
+    is_valid = True
+    for block_index in range(len(blockchain)):
+        if block_index == 0:
+            continue
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+    return is_valid 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
+    print('h: Manipulate the blockchain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
@@ -52,9 +68,18 @@ while True:
         add_transaction(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_element()
+    elif user_choice == 'h':
+        if len(blockchain) >= 1:
+            blockchain[0] = [2] 
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Invalid Input! Please pick a value from the list!')
+    if not verify_chain():
+        print_blockchain_element()
+        print('Blockchain not valid!')
+        break
+else: 
+    print('User left!')
 
 print('Done!')
